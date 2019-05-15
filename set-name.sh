14e8dd95eb40851307bb46f4b7552f7454f3ab9d
Sander Moolin <sandermoolin@gmail.com>
2019-05-15 00:24:29 -0400

    Other naming fixes

--- set-name.sh
+++ set-name.sh
@@ -1,3 +1,3 @@
 echo "\033[0;32mEnter your component name:\033[0m"
 read newName
-find ./ -type f -not -path "*node_modules*" -exec sed -i -e "s/your-component-name/$newName/g"
+find ./ -type f -not -path "*node_modules*" -exec sed -i -e "s/your-component-name/$newName/g" {} \;
