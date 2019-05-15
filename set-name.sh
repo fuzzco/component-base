75c24dd1c962738d2a1ccffa124a5267b222b2bc
Sander Moolin <sandermoolin@gmail.com>
2019-05-15 00:41:44 -0400

    Added note about removing script

--- set-name.sh
+++ set-name.sh
@@ -3,4 +3,4 @@ read kebab
 echo "\033[0;32mEnter your component name in PascalCase:\033[0m"
 read pascal
 LC_ALL=C find ./ -type f -not -path "*node_modules*" -not -path "*.git*" -exec sed -i '' -e "s/your-component-name/$kebab/g" {} \; -exec sed -i '' -e "s/YourComponentName/$pascal/g" {} \;
-echo "\033[0;32mDone! Component renamed successfully.\033[0m"
+echo "\033[0;32mDone! Component renamed successfully. Removing naming script.\033[0m"
