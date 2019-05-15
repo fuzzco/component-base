9a2dc9117611ecf08b712357e3d21c2a401d8731
Sander Moolin <sandermoolin@gmail.com>
2019-05-15 00:40:51 -0400

    Working naming function

--- set-name.sh
+++ set-name.sh
@@ -1,3 +1,6 @@
-echo "\033[0;32mEnter your component name:\033[0m"
-read newName
-find ./ -type f -not -path "*node_modules*" -exec sed -i -e "s/your-component-name/$newName/g" {} \;
+echo "\033[0;32mEnter your component name in kebab-case:\033[0m"
+read kebab
+echo "\033[0;32mEnter your component name in PascalCase:\033[0m"
+read pascal
+LC_ALL=C find ./ -type f -not -path "*node_modules*" -not -path "*.git*" -exec sed -i '' -e "s/your-component-name/$kebab/g" {} \; -exec sed -i '' -e "s/YourComponentName/$pascal/g" {} \;
+echo "\033[0;32mDone! Component renamed successfully.\033[0m"
