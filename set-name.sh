4b6b9f40ddd714f1499f1d77530dab7d8cbd4d01
Sander Moolin <sandermoolin@gmail.com>
2019-05-16 10:46:46 -0400

    Renaming vue component in naming script

--- set-name.sh
+++ set-name.sh
@@ -2,5 +2,8 @@ echo "\033[0;32mEnter your component name in kebab-case:\033[0m"
 read kebab
 echo "\033[0;32mEnter your component name in PascalCase:\033[0m"
 read pascal
+# replace component names in files
 LC_ALL=C find ./ -type f -not -path "*node_modules*" -not -path "*.git*" -exec sed -i '' -e "s/your-component-name/$kebab/g" {} \; -exec sed -i '' -e "s/YourComponentName/$pascal/g" {} \;
+# update file name
+mv ./src/YourComponentName.vue "$pascal.vue"
 echo "\033[0;32mDone! Component renamed successfully. Removing naming script.\033[0m"
